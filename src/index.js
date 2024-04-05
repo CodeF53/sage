6bc8362dabd8d01d98c134170e16b23483c0ee32
CodeF53 <fseusb@gmail.com>
2024-04-04 19:54:17 -0600

    lint files I forgot about, fix weed obsession

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
-import { ShardingManager, Events } from "discord.js";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { Logger, LogLevel } from "meklog";
-import dotenv from "dotenv";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { Events, ShardingManager } from 'discord.js'
+import { LogLevel, Logger } from 'meklog'
 
-dotenv.config();
+const production = process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production'
+const log = new Logger(production, 'Shard Manager')
 
-const production = process.env.NODE_ENV == "prod" || process.env.NODE_ENV == "production";
-const log = new Logger(production, "Shard Manager");
+log(LogLevel.Info, 'Loading')
 
-log(LogLevel.Info, "Loading");
+const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), 'bot.js')
+const manager = new ShardingManager(filePath, { token: process.env.TOKEN })
 
-const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), "bot.js");
-const manager = new ShardingManager(filePath, { token: process.env.TOKEN });
+manager.on('shardCreate', async (shard) => {
+  const shardLog = new Logger(production, `Shard #${shard.id}`)
 
-manager.on("shardCreate", async shard => {
-	const shardLog = new Logger(production, `Shard #${shard.id}`);
+  shardLog(LogLevel.Info, 'Created shard')
 
-	shardLog(LogLevel.Info, "Created shard");
+  shard.once(Events.ClientReady, async () => {
+    shard.send({ shardID: shard.id, logger: shardLog.data })
 
-	shard.once(Events.ClientReady, async () => {
-		shard.send({ shardID: shard.id, logger: shardLog.data });
+    shardLog(LogLevel.Info, 'Shard ready')
+  })
+})
 
-		shardLog(LogLevel.Info, "Shard ready");
-	});
-});
-
-manager.spawn();
-
+manager.spawn()
